df62b4f99830fbd216a4083064f35728f85c940a
Jose Cuevas <josecueram@gmail.com>
2020-04-09 20:53:35 -0500

    added XMLHttpRequest example

--- src/js/home.js
+++ src/js/home.js
@@ -1,8 +1,22 @@
-console.log('hola mundo!');
-const noCambia = "Leonidas";
+// ? XMLHttpRequest
 
-let cambia = "@LeonidasEsteban"
+// * jQuery
+$.ajax('https://randomuser.me/api/', {
+  method: 'GET',
+  success: function(data) {
+    console.log('[jQuery]', data.results[0])
+  },
+  error: function(error) {
+    console.log('[jQuery]', error)
+  }
+})
 
-function cambiarNombre(nuevoNombre) {
-  cambia = nuevoNombre
-}
+// * Vanilla
+fetch('https://randomuser.me/api/')
+.then(response => {
+  return response.json()
+})
+.then(data => {
+  console.log('[Vanilla]', data.results[0])
+})
+.catch(error => console.log('[Vanilla]', error))
