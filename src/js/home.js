9536732f2ae13f945f452a9cbb9fe339de24d35a
Jose Cuevas <josecueram@gmail.com>
2020-04-09 21:57:39 -0500

    added DOM Elements Selectors

--- src/js/home.js
+++ src/js/home.js
@@ -1,16 +1,30 @@
 
-(async function movies () {
-  const getMovies = async genre => {
-    const url = `https://yts.mx/api/v2/list_movies.json?genre=${genre}/`
+(async function pokedex () {
+  const getPokemons = async type => {
+    const url = `https://pokeapi.co/api/v2/type/${type}/`
 
     const response = await fetch(url)
     const data = await response.json()
-    const movies = data.data
-    return movies
+    const pokemons = data.pokemon
+    return pokemons
   }
 
-  const actionMovies = await getMovies('action')
-  const terrorMovies = await getMovies('terror')
-  console.log("movies -> terrorMovies", terrorMovies)
-  console.log("movies -> actionMovies", actionMovies)
+  const grassPokemon = await getPokemons('grass')
+  console.log("pokedex -> grassPokemon", grassPokemon)
+
+ 
+
+  const $modal = document.querySelector('#modal')
+  const $modalTitle = $modal.querySelector('h1')
+  const $modalImage = $modal.querySelector('img')
+  const $modalDescription = $modal.querySelector('p')
+  const $hideModal = document.querySelector('#hide-modal')
+  const $overlay = document.querySelector('#overlay')
+  const $fireContainer = document.querySelector('#fire')
+  const $grassContainer = document.querySelector('#grass')
+  const $waterContainer = document.querySelector('#water')
+  const $featuringContainer = document.querySelector('#featuring')
+  const $form = document.querySelector('#form')
+  const $home = document.querySelector('#home')
+
 })()
